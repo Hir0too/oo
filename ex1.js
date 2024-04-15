4efabebd457b702fa8ed35f2dff5cd3a1570ce76
Hir0too <163781411+Hir0too@users.noreply.github.com>
2024-04-15 12:17:10 +0200

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,13 @@
+
+
+
+(function() {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
+
+  byeSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})();
